c0c6b8df45b822a7228bbcca9a8db30f5d562000
Bruno do N. Maciel <brunodonascimentomaciel@gmail.com>
2019-11-10 09:06:46 -0300

    fix errors

--- shuvisco/install.sh
+++ shuvisco/install.sh
@@ -6,7 +6,7 @@ echo " "
 ### LOGIC ####
 
 # Arquivo de configuracao do cliente
-server='"SERVER="ec2-18-229-150-184.sa-east-1.compute.amazonaws.com"' 
+server="SERVER=\"ec2-18-229-150-184.sa-east-1.compute.amazonaws.com\"" 
 # Verificar se o arquivo de configuracao ja existe
 cat /etc/shuvisco.conf
 if [ $? -ne 0 ]
@@ -16,7 +16,7 @@ then
     chmod +x /etc/shuvisco.conf
 else
     # Arquivo ja existe
-    sed -i "/SERVER/c\\$server" /etc/serveo.conf
+    sed -i "/SERVER/c\\$server" /etc/shuvisco.conf
 fi
 
 # Mover o script de inicializacao do Serveo:
@@ -34,7 +34,7 @@ chmod +x /root/shuvisco/send.sh
 
 crontab -l > mycron
 # Verificando se ja existe algo no crontab referene ao shuvisco
-grep -i "shuvisco" install.sh
+grep -i "shuvisco" mycron
 if [ $? -ne 0 ]
 then
     # Nenhum rastro de versao anterior instalada
@@ -42,8 +42,8 @@ then
     echo "00 00 * * * /root/shuvisco/send.sh" >> mycron
 else
     # Vestigios encontrados
-    sed -i "/shuvisco/c\\* * * * * /root/shuvisco/tester.sh" mycron
-    sed -i "/shuvisco/c\\00 00 * * * /root/shuvisco/send.sh" mycron
+    sed -i "/tester.sh/c\\* * * * * /root/shuvisco/tester.sh" mycron
+    sed -i "/send.sh/c\\00 00 * * * /root/shuvisco/send.sh" mycron
 fi
 
 crontab mycron
